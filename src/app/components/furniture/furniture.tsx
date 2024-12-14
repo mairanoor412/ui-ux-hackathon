03f7579719661072a87f3c6cc794079c292de317
Maira Naeem <mairanoor412@gmail.com>
2024-12-14 17:10:21 +0500

    commit changes

--- src/app/components/furniture/furniture.tsx
+++ src/app/components/furniture/furniture.tsx
@@ -10,51 +10,51 @@ const Furniture = ()=>{
                 <h1 className="text-[16px] sm:text-[20px] md:text-[25px] font-[700] xl:text-[40px] "> #FuniroFurniture </h1>
             </div>
             {/* container */}
-            <div className="grid grid-cols-5 grid-rows-2 gap-4">
+            <div className=" grid grid-cols-12 grid-rows-2 gap-1 sm:gap-2 lg:gap-3  xl:h-auto">
+
                  {/* 1 */}
-                
-                <div className="grid row-span-1 place-items-end">
-                    <Image src="/image/furniture/1-Rectangle 36.png" alt="image" width={274} height={382} className="w-[274px] h-[382px]"  />
+                <div className="grid row-span-1 place-items-end col-span-1">
+                    <Image src="/image/furniture/1-Rectangle 36.png" alt="image" width={274} height={382} className="lg:w-[274px] h-[127px] sm:h-[191px] md:h-[271px] lg:h-[382px]"  />
                 </div>
 
                  {/* 2 */}
-                 <div className="grid row-span-1 place-items-end">
-                    <Image src="/image/furniture/2-Rectangle 38.png" alt="image" width={451} height={312} className="w-[451px] h-[312px] " />
+                 <div className="grid row-span-1 place-items-end col-span-4">
+                    <Image src="/image/furniture/2-Rectangle 38.png" alt="image" width={470} height={312} className="w-[470px] h-[104px] sm:h-[156px] md:h-[236px] lg:h-[312px] " />
                 </div>
 
                  {/* 3 */}
-                 <div className="row-span-2 grid place-items-center">
-                    <Image src="/image/furniture/3-Rectangle 40.png" alt="image" width={295} height={392} className="w-[295px] h-[392px]" />
+                 <div className="row-span-2 grid place-items-center col-span-2">
+                    <Image src="/image/furniture/3-Rectangle 40.png" alt="image" width={295} height={392} className="w-[295px] h-[130px] sm:h-[196px] md:h-[276px] lg:h-[392px]" />
                 </div>
 
                  {/* 4 */}
-                 <div className="grid row-span-1 place-items-end">
-                    <Image src="/image/furniture/4-Rectangle 43.png" alt="image" width={290} height={348} className="w-[290px] h-[348px]" />
+                 <div className="grid row-span-1 place-items-end col-span-3">
+                    <Image src="/image/furniture/4-Rectangle 43.png" alt="image" width={350} height={348} className="w-[350px] h-[116px] sm:h-[174px] md:h-[254px] lg:h-[348px]" />
                 </div>
 
                  {/* 5 */}
-                 <div className="grid row-span-1 place-items-end">
-                    <Image src="/image/furniture/5-Rectangle 45.png" alt="image" width={425} height={433} className="W-[425PX] H-[433PX]"/>
+                 <div className="grid row-span-1 place-items-end col-span-2 ">
+                    <Image src="/image/furniture/5-Rectangle 45.png" alt="image" width={425} height={433} className="W-[425PX] h-[144px] sm:h-[216px] md:h-[296px] lg:h-[433PX]"/>
                 </div>
 
                  {/* 6 */}
-                 <div>
-                    <Image src="/image/furniture/6-Rectangle 37.png" alt="image" width={381} height={323} className="w-[381px] h-[323px]"/>
+                 <div className="col-span-2">
+                    <Image src="/image/furniture/6-Rectangle 37.png" alt="image" width={381} height={323} className="w-[381px] h-[107px] sm:h-[161px] md:h-[241px] lg:h-[323px]"/>
                 </div>
 
                  {/* 7 */}
-                 <div>
-                    <Image src="/image/furniture/7-Rectangle 39.png" alt="image" width={344} height={242} className="w-[344px] h-[242px]"/>
+                 <div className="col-span-3">
+                    <Image src="/image/furniture/7-Rectangle 39.png" alt="image" width={344} height={242} className="w-[344px] h-[80px] sm:h-[121px] md:h-[201px] lg:h-[242px]"/>
                 </div>
 
                  {/* 8 */}
-                 <div>
-                    <Image src="/image/furniture/8-Rectangle 41.png" alt="image" width={178} height={242} className="w-[178px] h-[242px]" />
+                 <div className="col-span-2">
+                    <Image src="/image/furniture/8-Rectangle 41.png" alt="image" width={230} height={242} className="w-[230px] h-[80px] sm:h-[121px] md:h-[201px] lg:h-[242px]" />
                 </div>
 
                  {/* 9 */}
-                 <div  className="grid row-span-1  ">
-                    <Image src="/image/furniture/9-Rectangle 44.png" alt="image" width={258} height={196} className="w-[258px] h-[196px]" />
+                 <div  className="grid row-span-1 col-span-2  ">
+                    <Image src="/image/furniture/9-Rectangle 44.png" alt="image" width={258} height={196} className="w-[258px] h-[65px] sm:h-[98px] md:h-[178px] lg:h-[196px]" />
                 </div>
 
                 
--- src/app/components/rooms/rooms.tsx
+++ src/app/components/rooms/rooms.tsx
@@ -5,21 +5,21 @@ import { LuDot } from "react-icons/lu";
 
 const Rooms = ()=>{
     return(
-        <div className="overflow-x-hidden w-full h-auto xl:h-[670px] flex flex-col justify-center items-center bg-[#FCF8F3]">
-        <div className=" grid sm:grid-cols-12 gap-10">
+        <div className="overflow-x-hidden w-full h-auto lg:h-[670px] flex flex-col justify-center items-center bg-[#FCF8F3]">
+        <div className=" grid grid-cols-12 gap-3 md:gap-5 lg:gap-10">
 
             {/* left data */}
             
-            <div className=" flex flex-col justify-center items-center sm:items-start sm:col-span-5  gap-10 py-10 px-20 sm:px-1 md:px-5 lg:px-8 xl:px-32">
+            <div className=" flex flex-col justify-center items-center min-w-[100px] col-span-5 gap-3 lg:gap-10 py-10 px-16 xl:px-32">
                 <div className="flex flex-col gap-5">
-                    <h1 className="text-[25px] font-[700] sm-text-[20px] sm:w-[200px] md:text-[40px] md:w-[300px] leading-[48px] text-[#3A3A3A]  lg:w-[422px]"> 50+ Beautiful rooms inspiration </h1>
-                    <p className="text-[12px]  font-[500] sm:text-[10px] sm:w-[200px] md:text-[16px] md:w-[300px] text-[#616161]  lg:w-[368px]"> Our designer already made a lot of beautiful prototipe of rooms that inspire you </p> 
+                    <h1 className="w-[110px] text-[12px] font-[700] sm:text-[20px]   md:text-[30px] sm:w-[190px] md:w-[300px] lg:w-[350px] lg:leading-[48px] text-[#3A3A3A]  xl:w-[422px]"> 50+ Beautiful rooms inspiration </h1>
+                    <p className="w-[120px] text-[8px]  font-[500] sm:text-[10px]   md:text-[12px] sm:w-[190px] md:w-[300px]  text-[#616161] lg:w-[334px] xl:w-[368px]"> Our designer already made a lot of beautiful prototipe of rooms that inspire you </p> 
+                    <button className=" w-[70px] py-[4px] text-[7px] rounded-[5px] sm:w-[88px] sm:py-[6px] sm:text-[8px] md:w-[118px] md:py-[9px] md:text-[12px] lg:w-[176px] lg:py-[12px] lg:text-[16px] bg-[#B88E2F]  font-[600] "> Explore More </button>
                 </div>
             </div>
             
-
             {/* image-1 */}
-            <div className="hidden sm:block  col-span-3 relative">
+            <div className="col-span-3 relative">
                 <div className="hidden xl:block">
                 <div className="flex flex-row items-end absolute bottom-0 left-[20px] ">
                 <div className="w-[217px] h-[130px] bg-secondary opacity-[75%]  flex flex-col justify-center items-center ">
@@ -38,12 +38,12 @@ const Rooms = ()=>{
                 </div>
                 </div>
 
-                <Image src="/image/rooms/Rectangle 24.png" alt="image" width={404} height={582} className="w-[404px] h-[582px]"/>
+                <Image src="/image/rooms/Rectangle 24.png" alt="image" width={404} height={582} className="w-[404px] h-[221px] sm:h-[291px] md:h-[] lg:h-[582px]"/>
             </div>
             
             {/* image-2 */}
-            <div className="hidden sm:block col-span-3">
-            <Image src="/image/rooms/Rectangle 25.png" alt="image" width={372} height={486} className="w-[372px] h-[486px]"/>
+            <div className="col-span-3">
+            <Image src="/image/rooms/Rectangle 25.png" alt="image" width={372} height={486} className="w-[372px] h-[173px] sm:h-[243px] md:h-[] lg:h-[486px]"/>
             <div className="flex flex-row pt-6 ">
             <LuDot className="size-10 text-[#B88E2F]" />
             <LuDot className="size-10 text-[#D8D8D8]"/>
@@ -53,8 +53,8 @@ const Rooms = ()=>{
             </div>
 
             {/* image-3 */}
-            <div className="hidden sm:block col-span-1 ">
-            <Image src="/image/rooms/Rectangle 26.png" alt="image" width={372} height={486} className="w-[372px] h-[486px]"/>
+            <div className="col-span-1 ">
+            <Image src="/image/rooms/Rectangle 26.png" alt="image" width={372} height={486} className="w-[372px] h-[173px] sm:h-[243px] md:h-[] lg:h-[486px]"/>
             </div>
 
 
@@ -66,3 +66,8 @@ const Rooms = ()=>{
 }
 
 export default Rooms;
+
+
+
+
+// sm:px-4 md:px-5
